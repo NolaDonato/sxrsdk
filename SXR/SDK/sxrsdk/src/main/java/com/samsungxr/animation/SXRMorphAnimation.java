78aed664f6bbad2e48ba8e33603be971174cc930
Nola Donato <nola.donato@samsung.com>
2019-06-24 18:47:14 -0700

    allow morph weights to not match target

--- SXR/SDK/sxrsdk/src/main/java/com/samsungxr/animation/SXRMorphAnimation.java
+++ SXR/SDK/sxrsdk/src/main/java/com/samsungxr/animation/SXRMorphAnimation.java
@@ -131,9 +131,9 @@ public final class SXRMorphAnimation extends SXRAnimation implements PrettyPrint
      */
     public void setTarget(SXRMeshMorph morph)
     {
-        int numweightsTarget = morph.getBlendShapeCount();
-        int numWeightsAnim = mKeyInterpolator.getKeySize() - 1;
-        if (numweightsTarget != numWeightsAnim)
+        int numweights = morph.getBlendShapeCount();
+        mMorphWeights = new float[numweights];
+        if (numweights != mKeyInterpolator.getKeySize() - 1)
         {
             Log.w(TAG,"The number blend weights on the target morph do not match the animation");
         }
